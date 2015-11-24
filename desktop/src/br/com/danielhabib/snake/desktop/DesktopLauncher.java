cc483ca67b4f8067fe32b639ac00919b5d79e683
danielhvs <danielhvs70@gmail.com>
2015-11-24 00:21:39 -0200

    Snake game with libgdx framework.

---
+++ desktop/src/br/com/danielhabib/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package br.com.danielhabib.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import br.com.danielhabib.snake.core.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
